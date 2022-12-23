8b93cf79e0692856a8b28bc8a4a55d7db313983a
carl kylebäck wennerlöf <carl.k-wennerlof@telia.com>
2022-12-23 15:35:10 +0100

    first commit

---
+++ wtf.php
@@ -0,0 +1,22 @@
+<?php
+// get the array of used pictures from the request parameter
+$used = json_decode($_POST['used']);
+
+// Change this to the path of your image folder
+$imageFolder = 'bilder';
+
+// Read the contents of the image folder
+$files = scandir($imageFolder);
+
+// Filter the list of files to only include png files and not already used pictures
+$pngFiles = array_filter($files, function($file) use ($used) {
+  return strtolower(pathinfo($file, PATHINFO_EXTENSION)) === 'png' && !in_array($file, $used);
+});
+
+// Choose a random png file
+$randomFile = $pngFiles[array_rand($pngFiles)];
+
+// Output the file path of the random picture
+echo $imageFolder . '/' . $randomFile;
+
+?>
